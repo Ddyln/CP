4290cdcbfb6bd5b86b02ca2c9b37fb59129412ef
Dang Duy Lan <ddlan99tn@gmail.com>
2023-06-19 10:54:22 +0700

    upd

--- Others/iphone.cpp
+++ Others/iphone.cpp
@@ -7,14 +7,14 @@ int ABCD, A, B, C, D;
 signed main() {
     freopen("IPHONE.INP", "r", stdin);
     freopen("IPHONE.OUT", "w", stdout);    
-    // cin >> ABCD;
-    // D = ABCD % 10;
-    // ABCD /= 10;
-    // C = ABCD % 10;
-    // ABCD /= 10;
-    // B =  ABCD % 10;
-    // ABCD /= 10;
-    // A = ABCD;    
-    // cout << 0 << ' ' << A * B * C * D + A - B - C + D;
+    cin >> ABCD;
+    D = ABCD % 10;
+    ABCD /= 10;
+    C = ABCD % 10;
+    ABCD /= 10;
+    B =  ABCD % 10;
+    ABCD /= 10;
+    A = ABCD;    
+    cout << 0 << ' ' << A * B * C * D + A - B - C + D;
     return 0;
 }
