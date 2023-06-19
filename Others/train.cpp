52114775999d9dbc3f06a9819d95a4d646d7065e
Dang Duy Lan <ddlan99tn@gmail.com>
2023-06-20 00:04:33 +0700

    upd

--- Others/train.cpp
+++ Others/train.cpp
@@ -33,13 +33,6 @@ signed main() {
         cout << "NO";
         return 0;
     }
-    for (int i = 0; i < a.size(); i++)
-        for (int j = i + 1; j < a.size(); j++)
-            if (a[i] == a[j] || b[i] == b[j]) {
-                while (true) {
-                    
-                }
-            }
     int i = 0, j = 0;
     stack <int> st;
     while (i < a.size() || j < b.size()) {
