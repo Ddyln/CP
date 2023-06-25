284833c6862a41d7e18c1da1a5cfa035b56b9853
Dang Duy Lan <ddlan99tn@gmail.com>
2023-06-25 17:45:35 +0700

    upd

--- Others/test.cpp
+++ Others/test.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
+int MOD = 1e9 + 7;
+vector <vector <int>> f, vis;
+int dfs(vector<int>& locations, int& u, int& finish, int fuel) {
+    if (vis[u][fuel])
+        return f[u][fuel];
+    vis[u][fuel] = 1;
+    f[u][fuel] = (u == finish);
+    if (fuel == 0)
+        return f[u][fuel];
+    for (int v = 0; v < locations.size(); v++)
+        if (v != u && fuel - abs(locations[u] - locations[v]) >= 0) {
+            (f[u][fuel] += dfs(locations, v, finish, fuel - abs(locations[u] - locations[v]))) %= MOD;
+        }
+    return f[u][fuel];
+}
 
-vector <int> a, b;
+int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+    f.resize(locations.size(), vector <int> (fuel + 1, 0));
+    vis.resize(locations.size(), vector <int> (fuel + 1, 0));
+    return dfs(locations, start, finish, fuel);
+}
 
 signed main() {
     ios_base::sync_with_stdio(false);
@@ -10,56 +29,7 @@ signed main() {
         freopen("test.inp", "r", stdin);
         freopen("test.out", "w", stdout);
     #endif
-    string s;
-    cin >> s;
-    s += '-';
-    int tmp = 0;
-    for (int i = 0; i < s.size(); i++)
-        if (s[i] == '-')
-            a.push_back(tmp), tmp = 0;
-        else 
-            tmp = tmp * 10 + s[i] - '0';
-    
-    cin >> s;
-    s += '-';
-    tmp = 0;
-    for (int i = 0; i < s.size(); i++)
-        if (s[i] == '-')
-            b.push_back(tmp), tmp = 0;
-        else 
-            tmp = tmp * 10 + s[i] - '0';
-    if (a.size() != b.size()) {
-        cout << "NO";
-        return 0;
-    }
-    int i = 0, j = 0;
-    stack <int> st;
-    while (j < b.size()) {
-        if (i >= a.size()) {
-            if (!st.size() || st.top() != b[j]) {
-                cout << "NO";
-                return 0;
-            }
-            st.pop();
-            j++;
-        }
-        else if (a[i] != b[j]) {
-            if (st.size() && st.top() == b[j]) {
-				st.pop();
-				j++;
-				continue;
-			}
-			while (i < a.size() && a[i] != b[j])
-				st.push(a[i++]);
-			if (i >= a.size()) {
-				cout << "NO";
-				return 0;
-			}
-			i++, j++;
-        }
-        else
-            i++, j++;
-    }
-    cout << "YES";
+    vector <int> locations = {22,74,92,86,12,68,64,19,79,10,69,13,62,18,87,88,33,96,78,73,57,42,91,17,55,26,27,67,60,46,72,41};
+    countRoutes(locations, 30, 29, 47);
     return 0;
 }
